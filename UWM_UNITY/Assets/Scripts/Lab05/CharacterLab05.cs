bc0211647113d520b659c6b40c82891ea8cd628c
Jakub Pietkiewicz <jakubpietkiewicz@outlook.com>
2023-11-13 16:12:59 +0100

    Fix rollback on Character Script
    Fix rollback

--- UWM_UNITY/Assets/Scripts/Lab05/CharacterLab05.cs
+++ UWM_UNITY/Assets/Scripts/Lab05/CharacterLab05.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveWithCharacterController : MonoBehaviour
+public class CharacterLab05 : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 8.0f;
-    private float jumpHeight = 1.0f;
+    public float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
     private float pushPower = 2.0f;
 
